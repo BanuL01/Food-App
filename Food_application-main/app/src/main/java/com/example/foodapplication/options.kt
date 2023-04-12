37c8442a8138d5f0e8b9178609768e323dd1277e
Banuji Liyanwala <banuji01liyanwala@gmail.com>
2023-04-12 12:59:40 +0530

    added data to database

--- Food_application-main/app/src/main/java/com/example/foodapplication/options.kt
+++ Food_application-main/app/src/main/java/com/example/foodapplication/options.kt
@@ -79,6 +79,32 @@ class options : AppCompatActivity() {
 
                     )
 
+                    val beef_Banh_Mi_Bowls = Meal(
+                        name = "Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber",
+                        category = "Beef",
+                        area = "Vietnamese",
+                        instructions = "Add'l ingredients: mayonnaise, siracha\r\n\r\n1\r\n\r\nPlace rice in a fine-mesh sieve and rinse until water runs clear. Add to a small pot with 1 cup water (2 cups for 4 servings) and a pinch of salt. Bring to a boil, then cover and reduce heat to low. Cook until rice is tender, 15 minutes. Keep covered off heat for at least 10 minutes or until ready to serve.\r\n\r\n2\r\n\r\nMeanwhile, wash and dry all produce. Peel and finely chop garlic. Zest and quarter lime (for 4 servings, zest 1 lime and quarter both). Trim and halve cucumber lengthwise; thinly slice crosswise into half-moons. Halve, peel, and medium dice onion. Trim, peel, and grate carrot.\r\n\r\n3\r\n\r\nIn a medium bowl, combine cucumber, juice from half the lime, \u00bc tsp sugar (\u00bd tsp for 4 servings), and a pinch of salt. In a small bowl, combine mayonnaise, a pinch of garlic, a squeeze of lime juice, and as much sriracha as you\u2019d like. Season with salt and pepper.\r\n\r\n4\r\n\r\nHeat a drizzle of oil in a large pan over medium-high heat. Add onion and cook, stirring, until softened, 4-5 minutes. Add beef, remaining garlic, and 2 tsp sugar (4 tsp for 4 servings). Cook, breaking up meat into pieces, until beef is browned and cooked through, 4-5 minutes. Stir in soy sauce. Turn off heat; taste and season with salt and pepper.\r\n\r\n5\r\n\r\nFluff rice with a fork; stir in lime zest and 1 TBSP butter. Divide rice between bowls. Arrange beef, grated carrot, and pickled cucumber on top. Top with a squeeze of lime juice. Drizzle with sriracha mayo.",
+                        mealThumb = "https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg",
+                        Ingredient1 = "Rice",
+                        Ingredient2 = "Onion",
+                        Ingredient3 = "Lime",
+                        Ingredient4 = "Garlic Clove",
+                        Ingredient5 = "Cucumber",
+                        Ingredient6 = "Carrots",
+                        Ingredient7 = "Ground Beef",
+                        Ingredient8 = "Soy Sauce",
+                        Measure1 = "White",
+                        Measure2 = "1",
+                        Measure3 = "1",
+                        Measure4 = "3",
+                        Measure5 = "1",
+                        Measure6 = "3 oz ",
+                        Measure7 = "1 lb",
+                        Measure8 = "2 oz ",
+                        source = "",
+                    )
+
+
 
                     MealDao.insertMeal(sweet_and_sour_pork, chicken_marengo)
                     val meals: List<Meal> = MealDao.getAll()
